300045ed288577d0a9510d05be2a6596d9de8f81
Jose Vergara <jo.vergaraa@duocuc.cl>
2023-01-13 04:13:48 -0300

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'page404',
+    loadChildren: () => import('./pages/page404/page404.module').then( m => m.Page404PageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'weather',
+    loadChildren: () => import('./pages/weather/weather.module').then( m => m.WeatherPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'winelist',
+    loadChildren: () => import('./pages/winelist/winelist.module').then( m => m.WinelistPageModule)
+  },
+  {
+    path: 'wineprofile/:id',
+    loadChildren: () => import('./pages/wineprofile/wineprofile.module').then( m => m.WineprofilePageModule)
+  },
+  {
+    path: 'conversor',
+    loadChildren: () => import('./pages/conversor/conversor.module').then( m => m.ConversorPageModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'page404'
+  },
+
+
+
+ 
+  
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/weather/weather.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.page.html',
+  styleUrls: ['./weather.page.scss'],
+})
+export class WeatherPage  {
+
+  pageTitle = 'weather';
+  todayDate = new Date()
+  weatherTemp:any
+  weatherName : any
+  weatherIcon:any
+  weatherDetails:any
+  constructor(public HttpClient:HttpClient) { 
+    this.loadData()
+  }
+
+ 
+   loadData(){
+    this.HttpClient.get(`${API_URL}/weather?q=${"Santiago"}&appid=${API_KEY}`).subscribe(results => {
+      console.log(results);
+      this.weatherTemp= results;
+      this.weatherName = results;
+      this.weatherIcon = results;
+
+      console.log(this.weatherTemp.main.temp);
+      console.log(this.weatherName.name);
+      console.log(this.weatherDetails.icon);
+      
+      
+    
+   })
+}
+}
